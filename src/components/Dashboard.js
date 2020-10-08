4aba2ae1aa265ed4dd61a573ba62d90ab1a054a5
philipd <philip.dickau@gmail.com>
2020-10-08 15:13:21 -0600

    Complete activity "Building Components as Classes"

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,60 @@
 import React, { Component } from "react";
-
+import Loading from "components/Loading";
+import Panel from "components/Panel";
 import classnames from "classnames";
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  state = {
+    loading: false,
+    focused: null,
+  };
 
-    return <main className={dashboardClasses} />;
+  selectPanel(id) {
+    this.setState(prev => ({focused: prev.focused !== null ? null : id }));
+  }
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    return (
+      <main className={dashboardClasses}>
+        {data
+          .filter((x) => this.state.focused ? x.id === this.state.focused : true)
+          .map((x) => (
+            <Panel onSelect={event => this.selectPanel(x.id)} key={x.id} label={x.label} value={x.value} />
+          ))}
+      </main>
+    );
   }
 }
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6,
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm",
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday",
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3",
+  },
+];
 
 export default Dashboard;
